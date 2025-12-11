4d8608eadf464a73786b8949e3e1eea7ca889694
дарья сюрмеева <dasasurmeeva@outlook.com>
2025-12-11 12:50:06 +0400

    Добавлен роут\ логин для получения данных /

--- cms/app.py
+++ cms/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-# Простой роут
-@app.route('/')
-def home():
-    return "Сервер работает! Hello, World!"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(f"Логин: {username}, Пароль: {password}")
+        return f"Вы ввели: {username}"
+    return render_template_string('''
+        <form method="POST" action="/login">
+            Логин: <input type="text" name="username"><br>
+            Пароль: <input type="password" name="password"><br>
+            <input type="submit" value="Войти">
+        </form>
+    ''')
 
 if __name__ == "__main__":
     app.run(debug=True)
